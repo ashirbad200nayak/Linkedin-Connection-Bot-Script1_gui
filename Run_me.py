3ed1e19bb91bdaf83307d666e2633e7a1faf3605
ashirbad200nayak <ashirbad200nayak@gmail.com>
2025-02-03 02:01:26 +0530

    Initial Commits

---
+++ Run_me.py
@@ -0,0 +1,10 @@
+#############################################
+__author__ = "Ashirbad Nayak"                  #
+__LinkedIn__ = "ashirbadnayak"                #
+__Github__ = "ashirbadnayak"                  #
+#############################################
+
+
+from Scripts import graphical_script
+if __name__ == '__main__':
+    graphical_script.guibuild()
